2b6ead57f91544ab4ef880e5a8b5769a1b4cfedd
Antonio Jefferson Moreira Freitas <jeffersonfreitas@unimedce.com.br>
2023-03-17 21:23:35 -0300

    Create Project

---
+++ src/main/java/com/jeffersonfreitas/App.java
@@ -0,0 +1,16 @@
+package com.jeffersonfreitas;
+
+import com.jeffersonfreitas.math.MathAdd;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        System.out.println( MathAdd.add(10.0, 10.0) );
+    }
+}
---
+++ src/main/java/com/jeffersonfreitas/math/MathAdd.java
@@ -0,0 +1,9 @@
+package com.jeffersonfreitas.math;
+
+public class MathAdd {
+
+  public static Double add (final Double a, final Double b){
+    return a + b;
+  }
+  
+}
