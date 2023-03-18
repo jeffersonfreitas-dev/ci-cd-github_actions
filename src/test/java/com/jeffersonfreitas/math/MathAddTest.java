8dffe86cd2402ecc38b91926d9acfc600b6b6e97
Antonio Jefferson Moreira Freitas <jeffersonfreitas@unimedce.com.br>
2023-03-18 09:16:49 -0300

    Teste MathAdd

---
+++ src/test/java/com/jeffersonfreitas/math/MathAddTest.java
@@ -0,0 +1,18 @@
+package com.jeffersonfreitas.math;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MathAddTest {
+
+  @Test
+  public void testMathAdd(){
+    final Double a = 10.0;
+    final Double b = 10.0;
+
+    final Double result = MathAdd.add(a, b);
+
+    Assertions.assertEquals(result, 20);
+  }
+  
+}
